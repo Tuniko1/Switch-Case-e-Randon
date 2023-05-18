066f95f02c137ddcf163996940c9d19b9712c0a6
arthurpereira <arthurpereira_terc@in.gov.br>
2023-05-18 12:32:35 -0300

    Alinhamento de syntaxes

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // Jogo de  dados
 		
-		/*Faça um programa para gerar 3 números aleatórios para 3 dados, mostre a soma dos números que saíram. 
-		 * Se 2 dados saírem com a mesma numeração o jogador ganha 2 pontos de bônus, 
-		 * Se os 3 dados saírem com a mesma numeração ele ganha 6 pontos de bônus e deve aparecer a mensagem "você é muito sortudo". 
-		 * Se o total da soma dos dados mais o bônus for menor que 15 o jogador perde e apresenta a mensagem "Lamento, mas você perdeu" , 
-		 * Se for maior ou igual a 15 ele ganha e apresenta a mensagem "parabéns, você ganhou!" .
+		/*FaÃ§a um programa para gerar 3 nÃºmeros aleatÃ³rios para 3 dados, mostre a soma dos nÃºmeros que saÃ­ram. 
+		 * Se 2 dados saÃ­rem com a mesma numeraÃ§Ã£o o jogador ganha 2 pontos de bÃ´nus, 
+		 * Se os 3 dados saÃ­rem com a mesma numeraÃ§Ã£o ele ganha 6 pontos de bÃ´nus e deve aparecer a mensagem "vocÃª Ã© muito sortudo". 
+		 * Se o total da soma dos dados mais o bÃ´nus for menor que 15 o jogador perde e apresenta a mensagem "Lamento, mas vocÃª perdeu" , 
+		 * Se for maior ou igual a 15 ele ganha e apresenta a mensagem "parabÃ©ns, vocÃª ganhou!" .
 		*/
 		
 		
@@ -22,39 +22,44 @@ public class Program {
 		
 		int bonus = 0;
 		int saldoBonus = 0;
-		int saldoBonus2 = 0;
+		
 		
 		int somaDado = (op + op2 + op3);
-		System.out.println("a soma dos dados é : "+op+" + "+op2+" + "+op3+" = "+somaDado);		
+		System.out.println("Iniciando Jogo de Dados -->");
+		System.out.println("a soma dos dados Ã© : "+op+" + "+op2+" + "+op3+" = "+somaDado);		
 		System.out.println("");
 		
-		/*switch (somaDado) {
-		case 1:*/ 
+		 
 			if (op == op2 || op == op3 || op2 == op3) {
 				saldoBonus = bonus + 2;
-			System.out.println("Você ganhou 2 pontos pelos dados iguais, seu saldo de Bônus é: "+saldoBonus+" PONTOS");	
+			System.out.println("VocÃª ganhou 2 pontos pelos dados iguais, seu saldo de BÃ´nus Ã©: +"+saldoBonus+" PONTOS");	
 			}
 			if(op == op2 && op == op2 && op2 == op3) {
-				saldoBonus2 = saldoBonus + 6;
-				System.out.println("você é muito sortudo!!");
+				saldoBonus = saldoBonus + 6;
+				System.out.println("vocÃª Ã© muito sortudo!!");}
 			if(somaDado + saldoBonus < 15) {
-				System.out.println("");
-				System.out.println("Lamento, mas você perdeu!");
-			}if(somaDado + saldoBonus >= 15) {
-				System.out.println("");		
-				System.out.println("Parabéns, você ganhou!");
-			}else {System.out.println("Nem ganhou nem perdeu - Dilma Rousseff");	
-			/*
-		case 2:			
-			System.out.println("Caso 2");
-			break;
+				System.out.println();
+				System.out.println("Soma dos Dados + Saldo de Bonus: "+(somaDado+saldoBonus+" Ã© MENOR que 15 :("));
+				System.out.println();
+				System.out.println("Lamento, mas vocÃª perdeu!   *Saldo Bonus: "+saldoBonus);
+			}else if(somaDado + saldoBonus == 15) {{
+				System.out.println();
+				System.out.println("ParabÃ©ns, Ganhou!!");
+				System.out.println();
+				System.out.println("Soma dos Dados + Saldo de Bonus: "+(somaDado+saldoBonus+" Ã© IGUAL a 15 :D"));
+
+			}
+			}else {
+			System.out.println();
+			System.out.println("ParabÃ©ns, Ganhou!!");
+			System.out.println();
+			System.out.println("Soma dos Dados + Saldo de Bonus: "+(somaDado+saldoBonus+" Ã© MAIOR que 15 :D"));
 			
-		default:*/
 			if (somaDado <= 0) {
 				
-			System.out.println(somaDado+" não é um número inteiro!!");	
+			System.out.println(somaDado+" nÃ£o Ã© um nÃºmero inteiro!!");	
 		
-			}}//if	
-	}//switch case
+			}//if	
+	}//else
 }//void main
 }//Program
